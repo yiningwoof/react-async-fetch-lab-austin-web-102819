258dba753f1896cbbd15d9a76aef8b281a3b4064
Yining Wang <yining.wang@utexas.edu>
2020-01-13 14:21:27 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 // create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	componentDidMount() {
+		fetch('http://api.open-notify.org/astros.json');
+	}
+	render() {
+		return <div></div>;
+	}
+}
+
+export default App;
